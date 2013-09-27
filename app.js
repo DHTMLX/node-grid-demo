58934b461ba4c5e8b00a4795ce0a82b18be59f7e
Stanislau Wolski <stanislau.wolski@gmail.com>
2013-09-27 18:57:55 +0300

    [update] changed to mysql DB

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
 var express = require('express');
 var path = require('path');
 
-var db = require('mongoskin').db("localhost/testdb", { w: 0});
-    db.bind('books');
+var mysql      = require('mysql');
+var db = mysql.createConnection({
+  host     : '192.168.3.251',
+  user     : 'sampleDB',
+  password : 'sampleDB',
+  database : 'sampleDB'
+});
 
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 
-
-app.get('/init', function(req, res){
-	db.books.insert({ 
-		sales:  1200, 
-		author: "Jon Duckett",
-		title:  "HTML and CSS: Design and Build Websites",
-		price:  "16.70"
-	});
-	db.books.insert({ 
-		sales:  -800, 
-		author: "Steve Krug",
-		title:  "Don't Make Me Think",
-		price:  "40.00"
-	});
-	db.books.insert({ 
-		sales:  1200, 
-		author: "Douglas Crockford",
-		title:  "JavaScript: The Good Parts",
-		price:  "15.50"
-	});
-	db.books.insert({ 
-		sales:  1200, 
-		author: "John Resig",
-		title:  " Secrets of the JavaScript Ninja",
-		price:  "23.99"
-	});
-
-	res.send("Test data was added to the database");
-});
-
-
 app.get('/data', function(req, res){
-	db.books.find().toArray(function(err, data){
-		//set id property for all records
-		for (var i = 0; i < data.length; i++)
-			data[i].id = data[i]._id;
+	db.query("SELECT * FROM books", function(err, rows){
+		if (err) console.log(err);
 		
-		//output response
-		res.send(data);
+		res.send(rows);
 	});
 });
 
@@ -58,27 +29,34 @@ app.post('/data', function(req, res){
 	var sid = data.gr_id;
 	var tid = sid;
 
-	delete data.id;
-	delete data.gr_id;
-	delete data["!nativeeditor_status"];
-
+	var sales  = data.sales;
+	var author = data.author;
+	var title  = data.title;
+	var price  = data.price;
 
 	function update_response(err, result){
-		if (err)
+		if (err){
+			console.log(err);
 			mode = "error";
+		}
+
 		else if (mode == "inserted")
-			tid = data._id;
+			tid = result.insertId;
 
 		res.setHeader("Content-Type","text/xml");
 		res.send("<data><action type='"+mode+"' sid='"+sid+"' tid='"+tid+"'/></data>");
 	}
 
 	if (mode == "updated")
-		db.books.updateById( sid, data, update_response);
+		db.query("UPDATE books SET sales = ?, author = ?, title = ?, price = ? WHERE id = ?",
+			[sales, author, title, price, sid],
+			update_response);
 	else if (mode == "inserted")
-		db.books.insert(data, update_response);
+		db.query("INSERT INTO books(sales, author, title, price) VALUES (?,?,?,?)",
+			[sales, author, title, price],
+			update_response);
 	else if (mode == "deleted")
-		db.books.removeById( sid, update_response);
+		db.query("DELETE FROM books WHERE id = ?", [sid], update_response);
 	else
 		res.send("Not supported operation");
 });
